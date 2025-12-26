472c0c93147be08f365f36bf5345f20da2d86a55
dmiry1378 <dima-degalab@yandex.ru>
2025-12-26 17:26:14 +0300

    Учебный проект Спринта 2 (Автогонки)

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,6 +1,38 @@
 
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Scanner scanner = new Scanner(System.in);
+        Race race = new Race();
+        for (int carNumber = 1; carNumber <= 3; carNumber++) {
+            int speed = 0;
+            System.out.println("Введите название машины №" + carNumber);
+            String name = scanner.next();
+            while (name.isEmpty()) {
+                System.out.println("Неверное название! Попробуйте снова.");
+                System.out.println("Введите название машины №" + carNumber);
+                name = scanner.next();
+            }
+
+            System.out.println("Введите скорость машины №" + carNumber);
+            if (scanner.hasNextInt()) {
+                speed = scanner.nextInt();
+            }
+            while (speed <= 0 || speed > 250) {
+                System.out.println("Неверная скорость! Попробуйте снова.");
+                System.out.println("Введите скорость машины №" + carNumber);
+                if (scanner.hasNextInt()) {
+                    speed = scanner.nextInt();
+                }
+                else {
+                    scanner.next();
+                }
+            }
+            race.checkLeader(new Car(name, speed)); // Проверяем новую машину на лидерство
+            System.out.println();
+        }
+        System.out.println("Самая быстрая машина: " + race.name);
+
     }
 }
